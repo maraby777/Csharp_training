36a53ec8ed655bb365ab357c6703fd500c44c91b
Natasha Tereshchenko <ntereshchenko@rescompany.com>
2019-10-10 00:50:32 +0200

    Ex.5 Create TestBase

--- addressbook-web-tests/Pages/Contacts.cs
+++ addressbook-web-tests/Pages/Contacts.cs
@@ -15,43 +15,12 @@ namespace addressbook_web_tests.Pages
         [Test]
         public void TheContactTest()
         {
-            navigator.OpenHomePage();
-            loginHelper.Login(new AccountData("admin", "secret"));
+            OpenHomePage();
+            Login(new Pages.AccountData("admin", "secret"));
             GoToNewContactForm();
             AddNewContact(new ContactData("Name " + DateTime.Now , "Surname", "test@test.com", "111222333"));
             GoToHomePage();
-            loginHelper.Logout();
-        }
-
-
-        private void GoToHomePage()
-        {
-            driver.FindElement(By.LinkText("home")).Click();
-        }
-
-        private void AddNewContact(ContactData contact)
-        {
-            driver.FindElement(By.Name("firstname")).Click();
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Name);
-            driver.FindElement(By.Name("theform")).Click();
-            driver.FindElement(By.Name("lastname")).Click();
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.Surname);
-            driver.FindElement(By.Name("theform")).Click();
-            driver.FindElement(By.Name("email")).Click();
-            driver.FindElement(By.Name("email")).Clear();
-            driver.FindElement(By.Name("email")).SendKeys(contact.Email);
-            driver.FindElement(By.Name("mobile")).Click();
-            driver.FindElement(By.Name("mobile")).Clear();
-            driver.FindElement(By.Name("mobile")).SendKeys(contact.Phone);
-            driver.FindElement(By.Name("theform")).Click();
-            driver.FindElement(By.XPath("//input[21]")).Click();
-        }
-
-        private void GoToNewContactForm()
-        {
-            driver.FindElement(By.LinkText("add new")).Click();
+            Logout();
         }
     }
 }
